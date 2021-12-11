18704534e96f9a25e9cf43f3a5c14af1d2b9f313
Mahbub <mahbub21alam2017@gmail.com>
2021-12-11 14:36:11 +0600

    Create join table as visits

--- schema.sql
+++ schema.sql
@@ -52,3 +52,10 @@ CREATE TABLE specializations (
     CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
     CONSTRAINT fk_vets FOREIGN KEY(vet_id) REFERENCES vets(id)
 );
+CREATE TABLE visits (
+   animal_id INT ,
+    vet_id INT,
+    date_of_visit DATE,
+    CONSTRAINT fk_animals FOREIGN KEY(animal_id) REFERENCES animals(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vet_id) REFERENCES vets(id)
+);
